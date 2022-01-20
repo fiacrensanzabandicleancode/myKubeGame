8cac34e76257b6721a770832b02db789b0ca9cd6
Fiacre Nsanzabandi <fayabobo@gmail.com>
2022-01-20 19:34:34 +0100

    Added some tutorial reminders that I did on youtube with Amigoscode

---
+++ game5/user-service-api/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json(
+    [
+        {
+            name: 'Bob',
+            email: 'bob@gmail.com'
+        },
+        {
+            name: 'Alice',
+            email: 'alice@gmail.com'
+        },
+        {
+            name: 'Jacke',
+            email: 'jacke@gmail.com'
+        }
+    ]
+  )
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
